fe54c20c726bf51cacee6a86ba48ca5f1d9e89c3
talhaabbas82 <talhaabbas860@gmail.com>
2025-05-20 13:21:26 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,21 @@
+console.log("welcome to my calculator");
+alert("welcome to my calculator");
+
+var num1=+prompt("Enter first number ");
+var num2=+prompt("Enter second number");
+console.log(num1+num2);
+
+var num3=prompt("Enter first number for subtract");
+var num4=prompt("Enter second number for subtract");
+console.log(num3-num4);
+
+var num5=prompt("Enter first number of multiply");
+var num6=prompt("Enter second number of multiply");
+console.log(num5*num6);
+
+var num7=prompt("Enter first number for divison");
+var num8=prompt("Enter second number for divison");
+console.log(num7/num8);
+
+alert("give me your feedback");
+alert("thanks to try the calculator");
